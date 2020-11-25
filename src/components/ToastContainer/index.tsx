ad96ed91c2e98554d49bcc336cdb464881e679cf
Lucas Mota <lucas.o.mota@gmail.com>
2020-11-25 09:24:48 -0300

    refactory(Toast):Separando o Toast e seu Container(que envia os dados ao mesmo)em dois componentes e ajustando uso de ícones para cada tipo de situação(error, info, success)

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,35 +1,17 @@
-import React, { useCallback } from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-import { Container, Toast } from './styles';
-import { ToastMessage, useToast } from '../../hooks/Toast';
+import React from 'react';
+import { Container } from './styles';
+import Toast from './Toast';
+import { ToastMessage } from '../../hooks/Toast';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
-
-  const handleRemoveToast = useCallback(
-    (id: string) => {
-      removeToast(id);
-    },
-    [removeToast],
-  );
-
   return (
     <Container>
       {messages.map(message => (
-        <Toast type={message.type} hasDescription={!!message.description}>
-          <FiAlertCircle size={20} />
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-          <button onClick={() => handleRemoveToast(message.id)} type="button">
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );
